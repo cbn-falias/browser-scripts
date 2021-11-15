9767fb0f8b120f523a5eaf2c4ba66563bb445d05
FAL <Tobias.Falkner@Combeenation.com>
2021-11-15 13:02:24 +0100

    Create MenuLounge Addons.user.js

---
+++ menuelounge/MenuLounge Addons.user.js
@@ -0,0 +1,68 @@
+// ==UserScript==
+// @name         MenuLounge Addons
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://menuelounge.com/*
+// @icon         https://www.google.com/s2/favicons?domain=menuelounge.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    copyAvailableProductsButtonToHeader();
+    showTodaysOrderInNavBar();
+})();
+
+function showTodaysOrderInNavBar() {
+    var year = new Date().getFullYear();
+    var month = new Date().getMonth() + 1;
+
+    $.ajax({
+        async: true,
+        url: "/my-account/orders/" + year + '-' + month,
+        method: "POST",
+        dataType: 'json',
+        success: function(response) {
+            var result = JSON.parse(response);
+            var day = new Date().getDate();
+            var dateString = String(day).padStart(2, '0') + '.' + String(month).padStart(2, '0') + '.' + year;
+            console.log(result);
+
+            var todaysOrder = result.orders.filter(order => {
+                return order.dateAndShift.indexOf(dateString) === 0;
+            });
+
+            todaysOrder = todaysOrder.map(order => order.product);
+
+            var navLinksAcc = document.getElementsByClassName('nav__links--account')[0];
+            var li = document.createElement('li');
+            var aEl = document.createElement('a');
+            li.appendChild(aEl);
+
+            aEl.title = 'Go to your orders';
+            aEl.href = '/de_AT/my-account/orders';
+            if(todaysOrder && todaysOrder.length > 0) {
+                aEl.textContent = 'Your order today: ' + todaysOrder.join(', ').replace('</br>', ', ');
+            } else {
+                aEl.textContent = 'No order found for today (by CBN addon)';
+            }
+            aEl.style.paddingRight = '12px';
+            navLinksAcc.appendChild(li);
+        }
+    })
+}
+
+function copyAvailableProductsButtonToHeader() {
+    var btn = document.querySelector('.carousel__component-show-all-link');
+    if(!btn || btn.length === 0) return;
+    btn = btn.cloneNode(true);
+
+    btn.classList.remove('btn');
+    btn.style.padding = '10px 15px';
+
+    var headerList = document.querySelector('.nav__links--products.js-offcanvas-links');
+    headerList.insertBefore(btn, headerList.firstChild);
+}
